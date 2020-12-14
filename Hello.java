c17c0597fa1294c20b67aeefddbc62320984aca2
[Arnapurna] <[bediarnapurna20@gmail.com]>
2020-12-15 00:16:30 +0530

    new branch commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello{
+    public static void main (String[] args){
+        System.out.println("Hello Arnapurna");
+
+        //add for loop
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println("n+i");
+        }
+
+        System.out.println("New branch added");
+        
+    }
+}

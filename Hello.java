eadcd11840ffdbd0af9afeded606da7de4388929
abc <abc@example.com>
2020-12-14 22:11:43 +0530

    Added update

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 class Hello{
     public static void main{
         System.out.println("Hello Arnapurna");
+
+        //add for loop
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println("n+i");
+        }
     }
 }
